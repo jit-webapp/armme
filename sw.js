ce238292b006884e11d514f2ab7b9fbd63658c62
jit-webapp <webapp.jit@gmail.com>
2026-03-24 13:19:15 +0700

    2.0

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'cute-notebook-v2';
+const urlsToCache = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
