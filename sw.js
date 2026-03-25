44b9a581e926eae221a3da5591464c8a06e6a431
jit-webapp <webapp.jit@gmail.com>
2026-03-25 14:56:15 +0700

    2.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cute-notebook-v2.2';
+const CACHE_NAME = 'armmynote-cache-v2.5';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
